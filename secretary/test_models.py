51c38da6d5670f0c6a1dd07edf565a513c5ec8df
Marina Melo <marinampires@gmail.com>
2016-03-27 23:43:16 -0300

    create contact model

--- secretary/test_models.py
+++ secretary/test_models.py
@@ -9,3 +9,8 @@ class ContactTestCase(TestCase):
     def test_find_contact_by_name(self):
         contact = Contact.objects.get(name="Neusa Melo")
         self.assertEqual(Contact.objects.first().name, contact.name)
+
+    def test_delete_contact_by_name(self):
+        Contact.objects.get(name="Neusa Melo").delete()
+        with self.assertRaises(Contact.DoesNotExist):
+        	Contact.objects.get(name="Neusa Melo")
