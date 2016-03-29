a4850bb2d01cec58f9faf34b3edad652b5455797
Marina Melo <marinampires@gmail.com>
2016-03-29 01:35:50 -0300

    add white noise for static files

--- postman/wsgi.py
+++ postman/wsgi.py
@@ -1,7 +1,10 @@
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "postman.settings")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(PROJECT_ROOT, 'staticfiles'))
