8ef65cabd5be8b727d9adbd2e513276ff7f12440
Marina Melo <marinampires@gmail.com>
2016-03-29 23:21:13 -0300

    Add django-rest-swagger

--- postman/urls.py
+++ postman/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import Locations, LocationDetail, index
 
 urlpatterns = [
 	url(r'^locations$', Locations.as_view(), name='locations'),
     url(r'^locations/(?P<postcode>[0-9]+)$', LocationDetail.as_view(), name='location-detail'),
     url(r'^locations/index$', index, name='locations-index'),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'', include('postman.urls')),
     url(r'', include('secretary.urls')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
