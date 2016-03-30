4edfc9744c733cdf032261a134328b1118911db2
Marina Melo <marinampires@gmail.com>
2016-03-29 22:50:34 -0300

    create contacts index page

--- secretary/urls.py
+++ secretary/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import Contacts, ContactDetail
+from .views import Contacts, ContactDetail, index
 
 urlpatterns = [
     url(r'^contacts$', Contacts.as_view(), name='contacts'),
     url(r'^contacts/(?P<pk>[0-9]+)$', ContactDetail.as_view(), name='contact-detail'),
+    url(r'^contacts/index$', index, name='contacts-index'),
 ]
