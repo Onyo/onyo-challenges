a20a70d93ab17bab9ecf45858b7f4ee4a2958178
Sérgio Berlotto Jr <sergio.berlotto@gmail.com>
2016-05-09 20:08:03 -0300

    Adding cep filter

--- src/bob/cep/views.py
+++ src/bob/cep/views.py
@@ -4,7 +4,12 @@ from cep.models import Cep
 from cep.serializers import CepSerializer
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import filters
 
 class CepViewSet(viewsets.ModelViewSet):
     queryset = Cep.objects.all()
     serializer_class = CepSerializer
+
+    filter_backends = (filters.DjangoFilterBackend,)
+
+    filter_fields = ('cep',)
