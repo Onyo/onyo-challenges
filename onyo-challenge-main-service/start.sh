2c7aefb78e44ecf1e00a685399c47e60189559e2
vimgas <victormoraesgs@hotmail.com>
2018-11-15 20:52:01 -0200

    force the sync

--- onyo-challenge-main-service/start.sh
+++ onyo-challenge-main-service/start.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations users
 python manage.py makemigrations api
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --run-syncdb
 python manage.py createcachetable
 python manage.py runserver 0.0.0.0:8000
