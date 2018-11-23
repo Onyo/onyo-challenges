33db61079d08df8802628f27bb9117cc49f0588e
Eric <eric.silva@solutis.com.br>
2018-11-23 19:25:49 -0300

    Create migrations

--- address_microservice/address_microservice/addresses/models.py
+++ address_microservice/address_microservice/addresses/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
